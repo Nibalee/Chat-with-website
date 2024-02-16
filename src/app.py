8ddece3673496e4125f0b777fb6251fa4944c0b4
Nibalee <benalinejd12@gmail.com>
2024-02-17 01:38:34 +0800

    CREATE RETRIEVER and RETRIEVAL CHAIN

--- src/app.py
+++ src/app.py
@@ -3,12 +3,18 @@ from langchain_core.messages import AIMessage, HumanMessage
 from langchain_community.document_loaders import WebBaseLoader
 from langchain.text_splitter import RecursiveCharacterTextSplitter
 from langchain_community.vectorstores import Chroma
-from langchain_openai import OpenAIEmbeddings, ChatOpenAI
+# from langchain_openai import OpenAIEmbeddings, ChatOpenAI
 from dotenv import load_dotenv
 from langchain_core.prompts import ChatPromptTemplate, MessagesPlaceholder
-from langchain.chains import create_history_aware_retriever
+from langchain_google_genai import GoogleGenerativeAIEmbeddings, ChatGoogleGenerativeAI
+import google.generativeai as genai
+from langchain.chains import create_history_aware_retriever, create_retrieval_chain
+from langchain.chains.combine_documents import create_stuff_documents_chain
+import os
 
 load_dotenv()
+
+genai.configure(api_key= os.getenv("GOOGLE_API_KEY"))
 
 def get_vectorstore_from_url(url):
     # get the text in document form
@@ -20,7 +26,8 @@ def get_vectorstore_from_url(url):
     document_chunks = text_splitter.split_documents(document)
 
     # create a vectorstore from the chunks
-    vector_store = Chroma.from_documents(document_chunks, OpenAIEmbeddings())
+    embeddings = GoogleGenerativeAIEmbeddings(model="models/embedding-001")
+    vector_store = Chroma.from_documents(document_chunks, embeddings)
 
     return vector_store
 
@@ -28,30 +35,38 @@ def get_response(user_input):
     return "I don't know"
 
 def get_context_retriever_chain(vector_store):
-    llm = ChatOpenAI()
+    llm = ChatGoogleGenerativeAI(model="gemini-pro")
 
     retriever = vector_store.as_retriever()
 
-    prompt = ChatPromptTemplate.from_messages({
+    prompt = ChatPromptTemplate.from_messages([
         MessagesPlaceholder(variable_name="chat_history"),
         ("user", "{input}"),
-        ("user", "Given the above conversation, generate a search query to look up in order to get information relevant to the conversation!")
-    })
+        ("user", "Given the above conversation, generate a search query to look up in order to get information relevant to the conversation!"),
+    ])
 
     retriever_chain = create_history_aware_retriever(llm, retriever, prompt)
 
     return retriever_chain
     
+def get_conversational_rag_chain(retriever_chain):
 
+    llm = ChatGoogleGenerativeAI(model="gemini-pro")
+
+    prompt = ChatPromptTemplate.from_messages([
+        ("system", "Answer the user's questions based on the below context: \n\n{context}"),
+        MessagesPlaceholder(variable_name="chat_history"),
+        ("user", "{input}"),
+    ])
+
+    stuff_document_chain = create_stuff_documents_chain(llm, prompt)
+
+    return create_retrieval_chain(retriever_chain, stuff_document_chain)
 
 #app config
 st.set_page_config(page_title="Chat with Websites", page_icon="🤖")
 st.title("Chat with Websites")
 
-if "chat_history" not in st.session_state:
-    st.session_state.chat_history = [
-        AIMessage(content="Hello, I am a bot. How can I help you?"),
-    ]
 
 #sidebar
 with st.sidebar:
@@ -62,23 +77,34 @@ if website_url is None or website_url == "":
     st.info("Please enter a Website URL!")
 
 else:
+    # session state
+    if "chat_history" not in st.session_state:
+        st.session_state.chat_history = [
+            AIMessage(content="Hello, I am a bot. How can I help you?"),
+        ]
 
-    vector_store = get_vectorstore_from_url(website_url)
+    if "vector_store" not in st.session_state:
+        st.session_state.vector_store = get_vectorstore_from_url(website_url)
 
-    retriever_chain = get_context_retriever_chain(vector_store)
+    # create conversation chain
+    retriever_chain = get_context_retriever_chain(st.session_state.vector_store)
+
+    conversation_rag_chain =  get_conversational_rag_chain(retriever_chain)
+
 
     #user input
     user_query = st.chat_input("Type your message here...")
     if user_query is not None and user_query != "":
-        response = get_response(user_query)
+        # response = get_response(user_query)
+        response = conversation_rag_chain.invoke({
+            "chat_history" : st.session_state.chat_history,
+            "input": user_query
+        })
+        st.write(response)
+        
         st.session_state.chat_history.append(HumanMessage(content=user_query))
         st.session_state.chat_history.append(AIMessage(content= response))
 
-        retrieved_documents = retriever_chain.invoke({
-            "chat_history": st.session_state.chat_history,
-            "input": user_query
-        })
-        st.write(retrieved_documents)
 
     #conversation
     for message in st.session_state.chat_history:
